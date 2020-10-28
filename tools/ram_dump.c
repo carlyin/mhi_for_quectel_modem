65448014c756af60d8210b37c3dce624a679cd23
carl.yin <carl.yin@quectel.com>
2020-10-28 13:57:36 +0800

    tools: Add ramdump test tool usage: make ./ramdump
    Signed-off-by: carl.yin <carl.yin@quectel.com>

---
+++ tools/ram_dump.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <getopt.h>
+
+//static const unsigned char edl_cmd[] = {0x4b, 0x65, 0x01, 0x00, 0x54, 0x0f, 0x7e};
+static const unsigned char dump_cmd1[] = {0x4b, 0x12, 0x18, 0x02, 0x01, 0x00, 0xd2, 0x7e};
+static const unsigned char dump_cmd2[] = {0x7e, 0x01, 0x04, 0x00, 0x4b, 0x25, 0x03, 0x00, 0x7e};
+static unsigned char rx_buf[2048];
+
+int main(int argc, char *argv[]) {
+	const char *mhi_dev = "0000:03:00.0";
+	char mhi_chan[64];
+	int fd;
+	int ret;
+	int opt;
+
+///lib/firmware/firehose/prog_firehose_sdx55.mbn
+	optind = 1;
+    while ( -1 != (opt = getopt(argc, argv, "f:h"))) {
+        switch (opt) {
+			case 'f':
+			break;
+			default:
+			break;
+		}
+    }
+
+	snprintf(mhi_chan, sizeof(mhi_chan), "/dev/mhi_%s_%s", mhi_dev, "SAHARA");
+	if (!access(mhi_chan, F_OK))
+		goto _find_sbl;
+
+	snprintf(mhi_chan, sizeof(mhi_chan), "/dev/mhi_%s_%s", mhi_dev, "DIAG");
+	fd = open(mhi_chan, O_RDWR | O_NOCTTY);
+	if (fd == -1) {
+		printf("fail to open %s, errno: %d (%s)\n", mhi_chan, errno, strerror(errno));
+		return 1;
+	}
+	printf("open('%s') = %d\n", mhi_chan, fd);
+
+	printf("send dump cmd\n");
+	write(fd, dump_cmd1, sizeof(dump_cmd1));
+	usleep(100*1000);
+	write(fd, dump_cmd2, sizeof(dump_cmd2));
+
+	ret = 1;
+	while (ret > 0) {
+		ret = read(fd, rx_buf, sizeof(rx_buf));
+		printf("read = %d\n", ret);
+	}
+
+	close(fd);
+
+	snprintf(mhi_chan, sizeof(mhi_chan), "/dev/mhi_%s_%s", mhi_dev, "SAHARA");
+	printf("wait %s\n", mhi_chan);
+	for (ret = 0; ret < 10; ret++) {
+		if (!access(mhi_chan, F_OK))
+			break;
+		sleep(1);
+	}
+
+_find_sbl:
+	fd = open(mhi_chan, O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if (fd == -1) {
+		printf("fail to open %s, errno: %d (%s)\n", mhi_chan, errno, strerror(errno));
+		return 2;
+	}
+	printf("open('%s') = %d\n", mhi_chan, fd);
+
+	close(fd);
+
+	symlinkat(mhi_chan, AT_FDCWD, "/dev/mhi_SAHARA");
+ 	system("./QLog -p /dev/mhi_SAHARA -s log123");
+	return ret;	
+}
